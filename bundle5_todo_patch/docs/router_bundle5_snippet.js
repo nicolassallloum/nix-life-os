54a42a4a782f8aab3baf22d6da9b896ca4b412f2
Nicolassallloum <nicolassallloum@gmail.com>
2026-07-09 16:20:07 +0300

    Remove Bundle 5 temporary patch artifacts

--- bundle5_todo_patch/docs/router_bundle5_snippet.js
+++
@@ -1,8 +0,0 @@
-// Add this route to your Vue router if there is not already a To-Do board route.
-// Adjust the import path if your router uses a different alias.
-{
-  path: '/todo/board',
-  name: 'TodoBoard',
-  component: () => import('@/pages/todo/TodoBoard.vue'),
-  meta: { requiresAuth: true, title: 'Task Organization' },
-}
